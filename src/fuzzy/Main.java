eb1d5ff6193a89f5db2aef9567d8f7b011c9b673
Lena <h.obertynska@gmail.com>
2020-12-22 14:15:53 +0100

    project done - zachowanie kotka(sen,jedzenie,atencja)

--- src/fuzzy/Main.java
+++ src/fuzzy/Main.java
@@ -14,10 +14,13 @@ public class Main {
 
            // int poziomNatezenia = Integer.parseInt(args[1]);
             // int poraDnia = Integer.parseInt(args[2]);
-            System.out.print("poziomNatezenia = ");
-            int poziomNatezenia = in.nextInt();
-            System.out.print("poziomNatezenia = ");
-            int poraDnia = in.nextInt();
+            System.out.println("WELCOME TO STEROWNIK KOTKIEM");
+            System.out.print("ilość snu (1-20h) = ");
+            int sen = in.nextInt();
+            System.out.print("ilość jedzenia (0-10) = ");
+            int jedzenie = in.nextInt();
+            System.out.print("ilość atencji (0-10) = ");
+            int atencja = in.nextInt();
 
 
 //wyswietl wykresy funkcji fuzyfikacji i defuzyfikacji
@@ -25,20 +28,17 @@ public class Main {
             fuzzyRuleSet.chart();
 
 //zadaj wartosci wejsciowe
-            fuzzyRuleSet.setVariable("poziom_natezenia", poziomNatezenia);
-            fuzzyRuleSet.setVariable("pora_dnia", poraDnia);
+            fuzzyRuleSet.setVariable("sen", sen);
+            fuzzyRuleSet.setVariable("jedzenie", jedzenie);
+            fuzzyRuleSet.setVariable("atencja", atencja);
 //logika sterownika
             fuzzyRuleSet.evaluate();
 
 //graficzna prezentacja wyjscia
-            fuzzyRuleSet.getVariable("zmiana_natezenia").chartDefuzzifier(true);
+            fuzzyRuleSet.getVariable("zachowanie").chartDefuzzifier(true);
 
 //System.out.println(fuzzyRuleSet);
 
-        } catch (ArrayIndexOutOfBoundsException ex) {
-            System.out.println("Niepoprawna liczba parametrow. Przyklad: java FuzzyExample string<plik_fcl> int<poziom natezenia> int<pora dnia>");
-        } catch (NumberFormatException ex) {
-            System.out.println("Niepoprawny parametr. Przyklad: java FuzzyExample string<plik_fcl> int<poziom natezenia> int<pora dnia>");
         } catch (Exception ex) {
             System.out.println(ex.toString());
         }
